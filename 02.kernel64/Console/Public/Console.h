b99a9c3e983f6af28022412fa89f303ef4fb00a0
Beomjun Kim <beomjun.kim@thirdpolygon.com>
2026-06-06 06:28:23 +0900

    ㅇㄸ

--- 02.kernel64/Console/Public/Console.h
+++ 02.kernel64/Console/Public/Console.h
@@ -7,8 +7,3 @@
 BOOL mintPrintString(int iX, int iY, const char *pcString, BYTE Attribute);
 
 #endif /* __CONSOLE_H__ */
-=======
-#ifndef __CONSOLE_H__
-#define __CONSOLE_H__
-#endif /* __CONSOLE_H__ */
->>>>>>> Stashed changes
